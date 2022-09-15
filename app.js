8c6297dbe29bb4683b50fcd9def1c5934c5980f8
Erick Alves <smurfvermelho123@gmail.com>
2022-09-15 19:54:13 -0300

    variável

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
-const numeroUm = 1;
-const numeroDois = 2;
+let lista = [1,2,3,4,5];
+let i = 0;
 
-console.log(numeroUm + numeroDois);
-
-const listaUm = [1,2,3,4,5,6];
-
-const cifrao = '\u26c4';
-
-console.log(cifrao);
-
-const cidade = 'belo horizonte';
-const inupt = 'BELO HORIZONTE';
-
-const inuptMinusculo = inupt.toLowerCase();
-
-console.log(cidade === inuptMinusculo);
-
-const aleatorio = 'ahsuid'
-
-console.log(aleatorio.length);
-
-for (let i = 0; i < aleatorio.length; i + 1){
-
-    i + 1
-
-    console.log('minha pika');
+for (let i = 0; i < lista.length; i += 1){
+    console.log(i);
 }
+
+
+function voltaZero(){
+
+    if (i > 5){
+
+        console.log('variável maior que 5')
+    } else{
+        console.log('não é maior que 5')
+    }
+}
+
+voltaZero();
