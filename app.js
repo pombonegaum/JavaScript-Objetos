697411a4e547c7ecf797eaa23f247ea1f43f595e
Erick Alves <smurfvermelho123@gmail.com>
2022-09-15 17:38:27 -0300

    fundamentos da aula

---
+++ app.js
@@ -0,0 +1,21 @@
+const numeroUm = 1;
+const numeroDois = 2;
+
+console.log(numeroUm + numeroDois);
+
+const listaUm = [1,2,3,4,5,6];
+
+const cifrao = '\u26c4';
+
+console.log(cifrao);
+
+const cidade = 'belo horizonte';
+const inupt = 'BELO HORIZONTE';
+
+const inuptMinusculo = inupt.toLowerCase();
+
+console.log(cidade === inuptMinusculo);
+
+const aleatorio = 'ahsuidhasuidhasudhasuidhasiudhasiudhaisudhasiu'
+
+console.log(aleatorio.length);
