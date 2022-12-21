6d49389936330a73706442181931610d3525ee7f
Erick Alves <smurfvermelho123@gmail.com>
2022-12-21 20:56:52 -0300

    .

---
+++ aula-objetos5/aula-Algoritmos1/listaLivros.js
@@ -0,0 +1,18 @@
+const precoLivros = [{
+titulo: "JavaScript",
+preco:25
+},
+{
+titulo:"PHP",
+preco:15
+},{
+    titulo:"Java",
+    preco:10
+},{
+titulo:"Elixir",
+preco:87
+},{
+titulo:"Python",
+preco:67}]
+
+module.export = precoLivros
